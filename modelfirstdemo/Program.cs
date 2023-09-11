b1cd81ea961dc4efe33214672e651695033e628b
student <student@MB>
2023-09-11 12:31:48 +0530

    Add project files.

---
+++ modelfirstdemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modelfirstdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
